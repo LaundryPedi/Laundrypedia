cd856844544345c4aea03c1f53ad1c76e005b5bb
nurasima <nrasima.wahid@gmail.com>
2018-05-24 01:56:19 +0800

    laundry
    penambahan masih error

--- app/src/main/java/com/example/nurasima/laundrypedia/MainKonfirm.java
+++ app/src/main/java/com/example/nurasima/laundrypedia/MainKonfirm.java
@@ -1,19 +1,34 @@
 package com.example.nurasima.laundrypedia;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Nurasima on 18/05/2018.
  */
 
 public class MainKonfirm extends AppCompatActivity {
+    @BindView(R.id.buttonberanda)Button buttonberanda;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.konfirmasi);
         ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.buttonberanda)
+    public void setKonfirm (View view){
+        Intent intent = new Intent(MainKonfirm.this,MainHome.class);
+        startActivity(intent);
     }
 }
